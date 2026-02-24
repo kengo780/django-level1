5dcb46f14a589db2e742fe7bd92dcf29d316780b
kengo780 <205727667+kengo780@users.noreply.github.com>
2026-02-24 09:34:33 +0000

    Djangoレベル1、4章の確認

--- django_level1/chapter03/views.py
+++ django_level1/chapter03/views.py
@@ -13,3 +13,45 @@ def work02(request):
 def work03(request):
     message = "<html><body><h1>今日は晴天</h1></body></html>"
     return HttpResponse(message)
+
+def work04(request):
+    message= """
+<html>
+    <body>
+        <h1>Fruits</h1>
+            <ul>
+                <li>apples</li>
+                <li>orange</li>
+                <li>melon</li>
+            </ul>
+    </body>
+</html>"""
+    return HttpResponse(message)
+
+def work05(request):
+    message="""
+<html>
+    <body>
+        <h1>生徒一覧</h1>
+            <ul>
+                <li>ジョン</li>
+                <li>ナンシー</li>
+                <li>ボブ</li>
+            </ul>
+    </body>
+</html>"""
+    return HttpResponse(message)
+
+
+def work06(request):
+    message="""
+<html>
+    <body>
+        <h2>今日の天気</h2>
+        <h2>晴れのち曇り</h2>
+        <h2>最高気温：30度</h2>P
+        <h2>最低気温：21度</h2>
+    </body>
+</html>
+"""
+    return HttpResponse(message)
